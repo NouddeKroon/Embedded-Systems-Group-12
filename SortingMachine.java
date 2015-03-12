30a19ec504d1294a85ce7e192eda5cced5a396f9
JurBartels <jur.bartels@hotmail.com>
2015-03-12 10:46:45 +0100

    Finalizig the java code, should be done now. added commentary.

--- SortingMachine.java
+++ SortingMachine.java
@@ -1,36 +1,50 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
 /**
- *
  * @author s137910
  */
 public class SortingMachine {
-    boolean Abort;
-    boolean StartStop;
-    boolean StopPressed;
-    boolean ColorWhite;
-    int ConveyorBelt;
-    int RotatingBuckets;
-    int LoadingArm;
-    int ColorLED;
+    boolean Abort;          //Stores the current value of the abort button.
+    boolean StartStop;      //Stores the current value of the Start/stop button.
+    boolean StopPressed;    //Remembers whether start/stop has been pressed so 
+                            //the program will halt on the next cycle.
+    boolean ColorWhite;     //Remebers if the color white has been detected.
+    
+    /**
+     * For each motor, store the current strength of output for the 
+     * motor in an int.                       
+     */
+    int ConveyorBelt;       
+    int RotatingBuckets;   
+    int LoadingArm;         
+    /**
+     *Stores an int for the current strength of output for the LED.
+     */
+    int ColorLED;          
     int PositionDetectorLED;
     int RotatingBucketsLED;
-    boolean WhiteBucketFront;
-    int Black;
-    int White;
-    int StateDisplay;
+    
+    boolean WhiteBucketFront;     //Holds the current position of the panel in 
+                                  //front of the bucket.
+    int Black;      //Holds the current amount of sorted black disks.
+    int White;      //Holds the current amount of sorted white disks.
+    int StateDisplay;  //Holds an int corresponding to a state.
+    /**
+     * Hold the current values of the different sensors in the system.
+     */
     boolean LoadingArmPS;
     boolean PositionDetectorSensor;
     boolean RotatingBucketsSensor;
     boolean ColorSensor;
-    int Clock1;
-    int previousInput;
-    int counter;
-    int[] outputs = new int[8];
+    int Clock1;        //Clock regulating timed actions in the program.
+    int previousInput; //Holds the previous input.
+    int counter;       //Counter used in the interrupt.
+    int DisplayCounter; //Counter used in settinf the display LEDs.
+    
+    /**
+     * Every method represents a state the machine can be in, and will under 
+     * certain conditions update different variables and jump to another state
+     * by calling the method corresponding to that state. 
+     */
     void RestingState(){
         while(true){
             if(Abort == true){
@@ -52,6 +66,7 @@ public class SortingMachine {
         ColorLED = 0;
         PositionDetectorLED = 0;
         StateDisplay = 98;
+        Abort = false;
         Abort98State();
         
     }
@@ -282,6 +297,7 @@ public class SortingMachine {
                 StateDisplay = 99;
                 Abort99State();
             }
+
             if(StopPressed == true) {
                 ConveyorBelt = 00;
                 PositionDetectorLED = 00;
@@ -301,70 +317,135 @@ public class SortingMachine {
        // The method will be called once every millisecond.
     void interrupt(){                  
         while(true){
-            int input = Read_Input();
+            int input = Read_Input();  //simulates reading the input as a binary number.
             
-            if ((previousInput & 1) == 0) {
+             //Isolates the second bit of the input and checks if it moved 
+            //from an unpressed to a pressed state.
+            if ((previousInput & 1) == 0) {      
                 if ((input & 1) != 0) {
                     StartStop = true;
+                    StopPressed = true;
                 }
+            } else {
+                StartStop = false;
             }
-            
-            if ((previousInput & (1 << 1)) == 0) {
+           
+            //Isolates the second bit of the input and checks if it moved
+            //from an unpressed to a pressed state.
+            if ((previousInput & (1 << 1)) == 0) {  
                 if ((input & (1 << 1)) != 0) {
                     Abort = true;
                 }
+            } else{
+                Abort = false;
             }
             
-            
-            if ((input & (1 << 2)) != 0) {
+            /**
+             * For each input from 2 to 5, isolate the corresponding bit, and, 
+             * if the signal is high, set the corresponding boolean to true, 
+             * otherwise, set it to false.
+             */
+            if ((input & (1 << 2)) != 0) {    
                 ColorSensor = true;
+            }else{
+                ColorSensor = false;
             }
-            
-             
             if ((input & (1 << 3)) != 0) {
                 PositionDetectorSensor = true;
+            }else{
+                PositionDetectorSensor = false;
             }
-            
-             
             if ((input & (1 << 4)) != 0) {
                 RotatingBucketsSensor = true;
+            }else{
+                RotatingBucketsSensor = false;
             }
-            
-             
             if ((input & (1 << 5)) != 0) {
                LoadingArmPS = true;
-            }
-            
-            
-            if(StartStop == true){
-               StopPressed = true;
+            }else{
+               LoadingArmPS = false;
             }
             
             previousInput = input;
             
            Clock1++;
            Set_Output_PWM();
-           Sleep(1);
+           SetDisplayLED();
+           
+           Sleep(1); //The interrupt is executed once every milisecond.
         }
     }
     
-    void Set_Output_PWM(){
-        int output = 0;
+    /**
+     * Controls output according to the PWM principle.
+     */
+    void Set_Output_PWM(){       
+        int output = 0;       //This is the word that represents the output.
         counter = counter + 10;
-        if (counter == 100) {
+        if (counter == 100) {  //Once counter reaches 100, reset back to 0.
             counter = 0;
         }
-        for (int i = 0; i <= 7; i++) {
-            if (outputs[i] <= counter) {
-                output = output ^ (1<<i);
-            }
+        
+        /**
+         * Check the output strength setting of each output.
+         * If the current value of the counter is lower than the brightness, the
+         * appropriate bit is flipped so that the corresponding output is high.
+         */
+        
+        if (LoadingArm <= counter) {
+            output = output ^ (1<<0);
         }
+        if (ConveyorBelt <= counter) {
+            output = output ^ (1<<1);
+        }
+        if (RotatingBuckets <= counter) {
+            output = output ^ (1<<2);
+        }
+        if (ColorLED <= counter) {
+            output = output ^ (1<<3);
+        }
+        if (PositionDetectorLED <= counter) {
+            output = output ^ (1<<4);
+        }
+        if (RotatingBucketsLED <= counter) {
+            output = output ^ (1<<5);
+        }
+        
         
         storeOutput(output);
     }
+    /**
+     * This method sets the correct numbers to the LED display on the PP2.
+     * The method SetDisplay() sets the numbers to the LEDs on the PP2, but can
+     * not be replicated in Java.
+     */
+    void SetDisplayLED(){
+        if(DisplayCounter == 0){
+            SetDisplay(0, Black % 10);
+        }
+        if(DisplayCounter == 1){
+            SetDisplay(1, (Black / 10));
+        }
+        if(DisplayCounter == 2){
+            SetDisplay(2, (StateDisplay % 10));
+        }
+        if(DisplayCounter == 3){
+            SetDisplay(3, (StateDisplay / 10));
+        }
+        if(DisplayCounter == 4){
+            SetDisplay(4, (White % 10));
+        }
+        if(DisplayCounter == 5){
+            SetDisplay(5, (White / 10));
+        }
+        
+        DisplayCounter++;
+        DisplayCounter = DisplayCounter % 6;
+    }
        
     void storeOutput(int output) {
-        //Simulates storing a word in the output position of PP2 storage.
+        //Simulates storing a word in the output position of PP2 storage (there
+        // is no java equivalent).
     }
     public static void main(String[] args) {
       new  SortingMachine().Initialize97State();
